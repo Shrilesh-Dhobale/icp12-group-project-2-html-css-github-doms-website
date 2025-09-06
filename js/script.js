b989cb14049375247cd6d1df86b072cccf257be5
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-06 21:53:42 +0530

    Html file created for login & sign up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 window.addEventListener('load', function() {
+    setTimeout(function() {
         document.getElementById('preloader').style.display = 'none';
-    });
+    }, 2000); // 2 seconds delay for testing
+});
