8a74bdcbf4e78f5778e6562e1be47f71a54b7a6c
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-07 09:45:18 +0530

    Authentication imported

--- js/login-signup.js
+++ js/login-signup.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
-
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
  
